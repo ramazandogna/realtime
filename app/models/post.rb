7ec169ed7327ad9353829989870f6b1da8bdd78e
Ramazan DOĞAN <doganrmzn40@gmail.com>
2025-07-31 18:41:08 +0300

    Turbo todo app

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+
+    after_create_commit {broadcast_prepend_to "posts"}
+    after_destroy_commit {broadcast_remove_to "posts"}
+
 end
